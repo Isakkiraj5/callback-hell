3d03f0e2d95d1c91dbe3656aa0a919a2a91e6ca6
Isakkiraj05 <rajisakki7@gmail.com>
2024-03-13 20:26:24 +0530

    changes

---
+++ js/script.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+    document.getElementById("countdownDisplay").innerHTML = 10;
+    setTimeout(() => {
+      document.getElementById("countdownDisplay").innerHTML = 9;
+      setTimeout(() => {
+        document.getElementById("countdownDisplay").innerHTML = 8;
+        setTimeout(() => {
+          document.getElementById("countdownDisplay").innerHTML = 7;
+          setTimeout(() => {
+            document.getElementById("countdownDisplay").innerHTML = 6;
+            setTimeout(() => {
+              document.getElementById("countdownDisplay").innerHTML = 5;
+              setTimeout(() => {
+                document.getElementById("countdownDisplay").innerHTML = 4;
+                setTimeout(() => {
+                  document.getElementById("countdownDisplay").innerHTML = 3;
+                  setTimeout(() => {
+                    document.getElementById("countdownDisplay").innerHTML = 2;
+                    setTimeout(() => {
+                      document.getElementById("countdownDisplay").innerHTML = 1;
+                      setTimeout(() => {
+                        document.getElementById("countdownDisplay").innerHTML = "Happy Independence Day!";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
+
